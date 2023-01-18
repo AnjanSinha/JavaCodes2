b17873da947d83a16718da5f3894d96bc14fe982
AnjanSinha <sinhaanjan2002@gmail.com>
2023-01-18 22:26:02 +0530

    Updated

--- src/SwitchCase.java
+++ src/SwitchCase.java
@@ -1,20 +1,26 @@
 public class SwitchCase {
     public static void main(String[] args) {
         int value = 2;
-        switch (value){
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3: case 4: case 5:
+        switch (value) {
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
                 System.out.println("Was 3 or 4 or 5");
-                System.out.println("Actually it was : "+ value);
-                break;
-            default:
-                System.out.println("Not 1, 2, 3, 4, 5 ");
-                break;
+                System.out.println("Actually it was : " + value);
+            }
+            default -> System.out.println("Not 1, 2, 3, 4, 5 ");
         }
+        String month = "JANUARY";
+        System.out.println("Month is in the " + getQuarter(month) + " quarter.");
+    }
+    public static String getQuarter(String month){
+        return switch (month) {         //Here the switch statement is being used as an expression.
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            default -> {
+                String badResponse = month + " is bad Input.";
+                yield badResponse;
+            }
+        };
     }
 }
